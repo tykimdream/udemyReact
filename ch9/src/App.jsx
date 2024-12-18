0335b5779fd031b7bc562e17d080201608d896dc
unknown <tykimdream@gmail.com>
2024-12-18 17:36:25 +0900

    task added

--- ch9/src/App.jsx
+++ ch9/src/App.jsx
@@ -3,61 +3,126 @@ import { useState } from 'react';
 import ProjectSidebar from './components/ProjectSidebar';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
 	const [projectsState, setProjectsState] = useState({
 		selectedProjectId: undefined,
-		projects: []
+		projects: [],
+		tasks: [],
 	});
 
+	const handleAddTask = (text) => {
+		setProjectsState((prevState) => {
+			const taskId = Math.random();
+			const newTask = {
+				text: text,
+				projectId: prevState.selectedProjectId,
+				id: taskId,
+			};
+			return {
+				...prevState,
+				tasks: [...prevState.tasks, newTask],
+			};
+		});
+	};
+
+	const handleDeleteTask = (id) => {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				tasks: prevState.tasks.filter((task) => task.id !== id),
+			};
+		});
+	};
+
+	const handleSelectProject = (id) => {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: id,
+			};
+		});
+	};
+
 	const handleStartAddProject = () => {
-		setProjectsState(prevState => {
+		setProjectsState((prevState) => {
 			return {
 				...prevState,
 				selectedProjectId: null,
 			};
-		})
-	}
+		});
+	};
 
 	const handleAddProject = (projectData) => {
-		const projectId = Math.random();
-		const newProject = {
-			...projectData,
-			id: projectId
-		}
-
-		setProjectsState(prevState => {
+		setProjectsState((prevState) => {
+			const projectId = Math.random();
+			const newProject = {
+				...projectData,
+				id: projectId,
+			};
 			return {
 				...prevState,
 				selectedProjectId: undefined,
-				projects: [...prevState.projects, newProject]
-			}
-		})
-	}
+				projects: [...prevState.projects, newProject],
+			};
+		});
+	};
 
 	const handleCancelAddProject = () => {
-		setProjectsState(prevState => {
+		setProjectsState((prevState) => {
 			return {
 				...prevState,
 				selectedProjectId: undefined,
-			}
-		})
-	}
+			};
+		});
+	};
 
+	const handleDeleteProject = () => {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+				projects: prevState.projects.filter(
+					(project) => project.id !== prevState.selectedProjectId
+				),
+			};
+		});
+	};
 
-	let content;
+	const selectedProject = projectsState.projects.find(
+		(project) => project.id === projectsState.selectedProjectId
+	);
+
+	let content = (
+		<SelectedProject
+			project={selectedProject}
+			onDelete={handleDeleteProject}
+			onAddTask={handleAddTask}
+			onDeleteTask={handleDeleteTask}
+			tasks={projectsState.tasks}
+		/>
+	);
 
 	if (projectsState.selectedProjectId === null) {
-		content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+		content = (
+			<NewProject
+				onAdd={handleAddProject}
+				onCancel={handleCancelAddProject}
+			/>
+		);
 	} else if (projectsState.selectedProjectId === undefined) {
-		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
 	}
 
 	return (
 		<main className='h-screen my-8 flex gap-8'>
 			<ProjectSidebar
 				onStartAddProject={handleStartAddProject}
-				projects={projectsState.projects} />
+				projects={projectsState.projects}
+				onSelectProject={handleSelectProject}
+				selectedProjectId={projectsState.selectedProjectId}
+			/>
 			{content}
 		</main>
 	);
---
+++ ch9/src/components/NewTask.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const NewTask = ({ onAdd, onDelete }) => {
+	const [enteredTask, setEnteredTask] = useState('');
+
+	const handleChange = (event) => {
+		setEnteredTask(event.target.value);
+	};
+
+	const handleClick = () => {
+		if (enteredTask.trim() === '') {
+			return;
+		}
+		onAdd(enteredTask);
+		setEnteredTask('');
+	};
+
+	return (
+		<div className='flex items-center gap-4'>
+			<input
+				type='text'
+				className='w-64 px-2 py-1 rounded-sm bg-stone-200'
+				onChange={handleChange}
+			/>
+			<button
+				className='text-stone-700 hover:text-stone-950'
+				onClick={handleClick}
+			>
+				Add Task
+			</button>
+		</div>
+	);
+};
+
+export default NewTask;
