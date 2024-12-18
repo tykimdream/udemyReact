e46722d821a6401d81c73d9254565c4ce065d93d
kihwan kim <tykimdream@naver.com>
2024-12-18 15:51:53 +0900

    modal

--- ch9/src/App.jsx
+++ ch9/src/App.jsx
@@ -1,11 +1,64 @@
+import { useState } from 'react';
+
 import ProjectSidebar from './components/ProjectSidebar';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
+
 function App() {
+	const [projectsState, setProjectsState] = useState({
+		selectedProjectId: undefined,
+		projects: []
+	});
+
+	const handleStartAddProject = () => {
+		setProjectsState(prevState => {
+			return {
+				...prevState,
+				selectedProjectId: null,
+			};
+		})
+	}
+
+	const handleAddProject = (projectData) => {
+		const projectId = Math.random();
+		const newProject = {
+			...projectData,
+			id: projectId
+		}
+
+		setProjectsState(prevState => {
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+				projects: [...prevState.projects, newProject]
+			}
+		})
+	}
+
+	const handleCancelAddProject = () => {
+		setProjectsState(prevState => {
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+			}
+		})
+	}
+
+
+	let content;
+
+	if (projectsState.selectedProjectId === null) {
+		content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+	} else if (projectsState.selectedProjectId === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+	}
+
 	return (
 		<main className='h-screen my-8 flex gap-8'>
-			{/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-			<ProjectSidebar></ProjectSidebar>
-			<NewProject />
+			<ProjectSidebar
+				onStartAddProject={handleStartAddProject}
+				projects={projectsState.projects} />
+			{content}
 		</main>
 	);
 }
